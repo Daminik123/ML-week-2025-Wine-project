a561df88d6965188cd8a245e193d9b36244b29fa
Dmytro <pavliv082017@gmail.com>
2025-01-31 00:54:09 +0200

    integrated trained_model to UI

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import pickle
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+import xgboost
+from xgboost import XGBRegressor
+
 
 from streamlit_scroll_to_top import scroll_to_here
+
+
+def predict_points(input_data):
+    new_df = pd.DataFrame([input_data])
+
+    regressor = pickle.load(open("trained_model.pkl", "rb"))
+    cv = pickle.load(open("vectorizer.pkl", "rb"))
+    ct = pickle.load(open("transformer.pkl", "rb"))
+
+    corpus2 = []
+    description2 = re.sub('[^a-zA-Z]', ' ', new_df['description'][0])
+    description2 = description2.lower().split()
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    all_stopwords.remove('not')
+    description2 = [ps.stem(word) for word in description2 if word not in set(all_stopwords)]
+    description2 = ' '.join(description2)
+    corpus2.append(description2)
+
+    words_array2 = cv.transform(corpus2).toarray()
+    words_df2 = pd.DataFrame(words_array2)
+
+    X2 = pd.concat([words_df2.reset_index(drop=True), new_df.reset_index(drop=True)], axis=1)
+    X_new2 = X2.drop(["description", "designation", "title", "winery"], axis=1)
+    X_new2 = X_new2.fillna(0)
+
+    X_new2.columns = pd.DataFrame(X_new2).columns.astype(str)
+    X_new2_transformed = np.array(ct.transform(X_new2))
+
+    predicted_score = regressor.predict(X_new2_transformed)
+
+    return predicted_score[0]
 
 
 if 'scroll_to_top' not in st.session_state:
@@ -34,10 +79,6 @@ price_input = st.number_input(label='Price', step=1., format='%.2f', placeholder
 
 province_input = st.text_input(label='Province', placeholder='The province or state that the wine is from')
 
-region1_input = st.text_input(label='Region1', placeholder='The wine growing area in a province or state (ie Napa)')
-
-region2_input = st.text_input(label='Region2', placeholder='Sometimes there are more specific regions specified within a wine growing area')
-
 taster_input = st.text_input(label='Taster name')
 
 title_input = st.text_input(label='Title', placeholder='The title of the wine review, which often contains the vintage')
@@ -61,10 +102,6 @@ if st.button("Submit", on_click=scroll_to_top):
         errors.append("Price must be greater than 0.")
     if not province_input:
         errors.append("Province is required.")
-    if not region1_input:
-        errors.append("Region1 is required.")
-    if not region2_input:
-        errors.append("Region2 is required.")
     if not taster_input:
         errors.append("Taster is required.")
     if not title_input:
@@ -84,16 +121,12 @@ if st.button("Submit", on_click=scroll_to_top):
             'designation': desig_input,
             'price': price_input,
             'province': province_input,
-            'region_1': region1_input,
-            'region_2': region2_input,
             'taster_name': taster_input,
             'title': title_input,
             'variety': variety_input,
             'winery': winery_input,
         }
 
-        input_df = pd.DataFrame([input_data])
-
-        predicted_points = 90 
+        predicted_points = predict_points(input_data)
 
         wine_points_placeholder.markdown(f"# Wine points is: **{predicted_points}**")
